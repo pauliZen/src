509efe2f0bf8b196a38f06b56ced3244ea95697a
lwang-astro <longwang.astro@gmail.com>
2015-05-07 22:28:20 +0200

    Update getexif.py

--- python/getexif.py
+++ python/getexif.py
@@ -16,7 +16,13 @@ fl = open('image.lst','r')
 path = fl.read()
 path = path.splitlines()
 
+print "FILE FocalLength[mm] Aperture Exposure[s] ISO Date Lens"
 for j in path:
-    info=get_exif(j)
-    print info
-    
+    data=get_exif(j)
+    print j,(data['FocalLength'][0]/data['FocalLength'][1]),data['ExposureTime'][0]/(data['ExposureTime'][1]+0.0), data['ISOSpeedRatings'],data['DateTime'],
+    if('ApertureValue' in data.keys()):
+        print data['ApertureValue'][0]/(data['ApertureValue'][1]+0.0),
+    if(42036 in data.keys()):
+        print data[42036]
+    else:
+        print
