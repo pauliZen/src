9f65f30629caae23e4db5c7639ea063912af4e98
lwang-astro <longwang.astro@gmail.com>
2015-05-19 10:35:52 +0200

    Add surfacedensity routine for Nbody

---
+++ Nbody/Tools/surfacedensity.cpp
@@ -0,0 +1,45 @@
+#include <cstdio>
+#include <cmath>
+
+// use bins
+//extern "C" void surfacedensity(int n, float* x, float* y, float* z, float* weight, float* rs, float xbin, float ybin, float xmin, float xmax, float ymin, float ymax) {
+//  
+//}  
+  
+extern "C" void surfacedensity(int n, float* x, float* y, float* weight, float* m, int rbin, float rmax, float rmin, float* rshell, float* rden, float* rm, int* rcount, float* rarea) {
+  float lmin=log10(rmin);
+  float scale=(float)rbin/(log10(rmax)-lmin);
+  for (int i=rbin-1;i>=0;i--) {
+    rden[i]=0.0;
+    rm[i]=0.0;
+    rshell[i]=pow(10.0,(i+1)/scale+lmin);
+    rcount[i]=0;
+    rarea[i]=3.1415926*rshell[i]*rshell[i];
+    if(i<rbin-1) rarea[i+1]=rarea[i+1]-rarea[i];
+  }
+  float rmin2=rmin*rmin;
+  float rmax2=rmax*rmax;
+  for (int i=0;i<n;i++) {
+    float r2=x[i]*x[i]+y[i]*y[i];
+    if (r2>rmin2) {
+      if (r2<rmax2) {
+        int nlr=(int)((0.5*log10(r2)-lmin)*scale);
+        if(nlr<rbin) {
+          //          if (r2>rshell[nlr]*rshell[nlr] || (nlr>0 && r2<rshell[nlr-1]*rshell[nlr-1])) printf("Not match: r=%f; rshell[i-1]=%f; rshell[i]=%f; nc=%d\n",sqrt(r2),rshell[nlr],rshell[nlr-1],nlr);
+          rden[nlr] +=weight[i];
+          rm[nlr] +=m[i];
+          rcount[nlr]++;
+        }
+      }
+    } else {
+      rden[0] +=weight[i];
+      rm[0] +=m[i];
+      rcount[0]++;
+    }
+  }
+  for (int i=0;i<rbin;i++) {
+    rden[i] /=rarea[i];
+    rm[i] /=rarea[i];
+  }
+}
+
---
+++ Nbody/Tools/surfacedensity.py
@@ -0,0 +1,81 @@
+import numpy as np
+import ctypes
+import sys
+
+def get_sd():
+    dll = ctypes.CDLL("/u/longw/lib/libsurfacedensity.so",mode=ctypes.RTLD_GLOBAL)
+    func = dll.surfacedensity
+    func.argtypes = [ctypes.c_int, ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float),
+                     ctypes.c_int, ctypes.c_float, ctypes.c_float,
+                     ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_float), ctypes.POINTER(ctypes.c_int), ctypes.POINTER(ctypes.c_float)]
+    return func
+
+__sd=get_sd()
+
+def sd(n,x,y,w,m,nbin,rmax,rmin,rshell,rden,rm,ncount,rarea):
+    xp  = x.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    yp  = y.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    wp  = w.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    mp  = m.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    rsp = rshell.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    rdp = rden.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    rmp = rm.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    ncp = ncount.ctypes.data_as(ctypes.POINTER(ctypes.c_int))
+    rap = rarea.ctypes.data_as(ctypes.POINTER(ctypes.c_float))
+    __sd(n,xp,yp,wp,mp,nbin,rmax,rmin,rsp,rdp,rmp,ncp,rap)
+
+
+larg=len(sys.argv)
+
+filename='data_prj'
+fout='sb'
+filter='V'
+rs=10
+rmax=20
+rmin=0.05
+nbins=15
+
+msun=4.83
+
+if (larg>=2):
+    filename=sys.argv[1]
+if (larg>=3):
+    filter=sys.argv[2]
+if (larg>=4):
+    rmax=float(sys.argv[3])
+if (larg>=5):
+    nbins=int(sys.argv[4])
+if (larg>=6):
+    rmin=float(sys.argv[5])
+if (larg>=7):
+    fout=sys.argv[6]
+if (larg>=8):
+    rs=float(sys.argv[7])
+
+fcol=18
+if (filter=='I'): fcol=21
+if (filter=='B'): fcol=20
+
+x,y,m1,m2,mag = np.loadtxt(filename,usecols=(1,2,9,10,fcol-1),unpack=True)
+
+dm=5.0*np.log10(rs)-5.0
+
+xp=x.astype(np.float32)
+yp=y.astype(np.float32)
+lp=(10.0**(0.4*(msun-(mag+dm)))).astype(np.float32)
+mp=(m1+m2).astype(np.float32)
+
+rshell=np.zeros(nbins).astype(np.float32)
+rden=np.zeros(nbins).astype(np.float32)
+rm=np.zeros(nbins).astype(np.float32)
+ncount=np.zeros(nbins).astype(np.int32)
+rarea=np.zeros(nbins).astype(np.float32)
+
+sd(mag.size,xp,yp,lp,mp,nbins,rmax,rmin,rshell,rden,rm,ncount,rarea)
+
+rmag=msun+21.572-2.5*np.log10(rden)
+rarc=rshell/rs*206265
+
+fhandle=open(fout,'w')
+np.savetxt(fhandle, np.transpose((rshell,rden,rarc,rmag,ncount,rm,rarea)),fmt="%f %f %f %f %d %f %f",header="r[pc] surface_brightness[L_{sun}/pc^2] r[arcsec] surface_brigtness[mag/arcsec^2] surface_number_density[1/pc^2] surface_mass_density[M_sun/pc^2] area[pc^2]",comments='')
+fhandle.close()
